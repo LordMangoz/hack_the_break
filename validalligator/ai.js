7dd038be7f0eee12775acafe3bb8383f385085f6
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-03-14 16:33:44 -0700

    implemented AI

--- validalligator/ai.js
+++ validalligator/ai.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
+
 async function getAIResponse(prompt) {
     const { GoogleGenAI } = await import("@google/genai");
-    
-    const ai = new GoogleGenAI({apiKey: process.env.API_KEY});
+    const ai = new GoogleGenAI({apiKey: "AIzaSyCQ7G6m1uGbJ4ldsFf7fyhVgD5IdJQe5xo"});
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.5-flash",
         contents: prompt,
     });
 
