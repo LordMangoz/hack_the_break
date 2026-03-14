f27cc6bf6c63c6569c98ba49b52a052021f09a49
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-03-14 12:51:41 -0700

    test

--- validalligator/extension.js
+++ validalligator/extension.js
@@ -10,21 +10,19 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "validalligator" is now active!');
-
-	
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('validalligator.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from ValidAlligator!');
+		const output = vscode.window.createOutputChannel('Valid Alligator');
+		output.appendLine('Hello World from Valid Alligator!');
+		output.show();
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+
 
 
 
@@ -35,4 +33,6 @@ module.exports = {
 	activate,
 	deactivate
 }
+
+
 
