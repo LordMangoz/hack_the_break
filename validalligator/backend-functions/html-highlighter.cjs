224c63bf5b770a41bc7d25be0746b7a3e357791a
umanga <umangabajgai@gmail.com>
2026-03-15 03:37:59 -0700

    continued debugging validator

--- validalligator/backend-functions/html-highlighter.cjs
+++ validalligator/backend-functions/html-highlighter.cjs
@@ -1,32 +1,37 @@
-const vscode = require('vscode'); // This is the vscode api
+const vscode = require("vscode"); // This is the vscode api
 
-const highlightWarningStyle = vscode.window.createTextEditorDecorationType
-({
-    isWholeLine: true,
-    backgroundColor: "rgba(255, 0, 0, .3)",
-    overviewRulerColor: "rgba(255, 0, 0, .3)",
-    overviewRulerLane: vscode.OverviewRulerLane.Full
-})
+const highlightWarningStyle = vscode.window.createTextEditorDecorationType({
+  isWholeLine: true,
+  backgroundColor: "rgba(255, 0, 0, .3)",
+  overviewRulerColor: "rgba(255, 0, 0, .3)",
+  overviewRulerLane: vscode.OverviewRulerLane.Full,
+});
 
-/**
- * @param {number} lineNum
- */
-function highlightWarning(lineNum)
-{
-    const problemLine = new vscode.Position(lineNum - 1, 0 /*this number does not matter*/) 
-    const editor = vscode.window.activeTextEditor;
+const warningLines = new Set();
 
-    if (!editor)
-        {
-            return
-        }
-        
-    const lineToHighlight = new vscode.Range(problemLine, problemLine);
-    const uri = editor.document.uri.toString();
-
-    editor.setDecorations(highlightWarningStyle, [lineToHighlight])
+function highlightWarning(lineNum) {
+  warningLines.add(lineNum);
 }
 
-module.exports =  {
-    highlightWarning,
+function applyHighlights() {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) return;
+
+  const ranges = [];
+
+  for (const line of warningLines) {
+    const pos = new vscode.Position(line - 1, 0);
+    const range = new vscode.Range(pos, pos);
+    ranges.push(range);
+  }
+
+  editor.setDecorations(highlightWarningStyle, ranges);
+
+  warningLines.clear();
+}
+
+module.exports = {
+  highlightWarning,
+  applyHighlights,
 };
